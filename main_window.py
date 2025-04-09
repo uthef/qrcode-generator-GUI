8055a7a35ea1872b929f7c4da9e34c9210ff0c05
Nightcrawler <denbrylski@gmail.com>
2025-04-09 19:30:35 +0300

    store temporary images in RAM instead of writing them to files

--- main_window.py
+++ main_window.py
@@ -1,18 +1,22 @@
+import qrcode.image.pil
 from main_window_layout import MainWindowLayout
 import os
 import tkinter
 import qrcode
+import io
 import qrcode.image.svg
-from pathlib import Path
 from PIL import Image
-from fsutils import QR_CODES_DIR, TEMP_DIR, make_sure_directory_exists
+from fsutils import QR_CODES_DIR, make_sure_directory_exists
 
 class MainWindow:
     def __init__(self, app):
+        self.logo_buffer: io.BytesIO = io.BytesIO()
+        self.img_buffer: io.BytesIO = io.BytesIO()
+
         self.app = app
         self.layout = MainWindowLayout(self)
-        make_sure_directory_exists(TEMP_DIR)
         self.layout.build()
+        self.layout.content_box.insert("end-1c", "temp")
         self.update_ui()
 
 
@@ -27,30 +31,38 @@ class MainWindow:
         qr.add_data(self.layout.content_box.get("1.0","end-1c")) # Gets content from row 1 idex 0 up to the end but deletes the last character (-1c) because using "end" always adds a linebreak at the end
 
         if mode == "temp": # If mode == "temp", save image to temp folder for use in the preview window
-            make_sure_directory_exists(TEMP_DIR)
-            
+            self.img_buffer = io.BytesIO()
+
             if self.layout.logo_checkbox.get() == 1: # generates qr code and adds logo if enabled
                 if self.layout.filetype_box.get() == ".jpg": img = qr.make_image(back_color=self.layout.background_color_box.get(),fill_color=self.layout.fill_color_box.get()).convert("RGB") # Converts to RGB without transparency as .jpg doenst support it
                 else: img = qr.make_image(back_color=self.layout.background_color_box.get(),fill_color=self.layout.fill_color_box.get()).convert("RGBA")
                 img = img.resize((400,400))
-                logo = Image.open(f"{TEMP_DIR}/logo.png").convert("RGBA")
-                logo = logo.resize((x:=20 * round(self.layout.logosize_slider.get()),y:= 20 * round(self.layout.logosize_slider.get())))
-                img.paste(logo, (200 - int(x/2),200 - int(y/2)),logo)
-                img.save(f"{TEMP_DIR}/temp.png")
+
+                try:
+                    logo = Image.open(self.logo_buffer).convert("RGBA")
+                    logo = logo.resize((x:=20 * round(self.layout.logosize_slider.get()),y:= 20 * round(self.layout.logosize_slider.get())))
+                    img.paste(logo, (200 - int(x/2),200 - int(y/2)),logo)
+                except:
+                    print("Error: invalid logo file")
+
+                img.save(self.img_buffer, format="PNG")
             else: # just generates qr code without adding the logo if disabled
                 img = qr.make_image(back_color=self.layout.background_color_box.get(),fill_color=self.layout.fill_color_box.get())
                 img = img.resize((400,400))
-                img.save(f"{TEMP_DIR}/temp.png")
-
+                img.save(self.img_buffer, format="PNG")
+    
         elif self.layout.logo_checkbox.get() == 1: # checks if logo addition is enabled
-            make_sure_directory_exists(TEMP_DIR)
-
             if self.layout.filetype_box.get() == ".jpg": img = qr.make_image(back_color=self.layout.background_color_box.get(),fill_color=self.layout.fill_color_box.get()).convert("RGB") # Converts to RGB without transparency as .jpg doenst support it
             else: img = qr.make_image(back_color=self.layout.background_color_box.get(),fill_color=self.layout.fill_color_box.get()).convert("RGBA")
             img = img.resize((2000,2000))
-            logo = Image.open(f"{TEMP_DIR}/logo.png").convert("RGBA")
-            logo = logo.resize((x:=100 * round(self.layout.logosize_slider.get()),y:= 100 * round(self.layout.logosize_slider.get())))
-            img.paste(logo, (1000 - int(x/2),1000 - int(y/2)),logo)
+
+            try:
+                logo = Image.open(self.logo_buffer).convert("RGBA")
+                logo = logo.resize((x:=100 * round(self.layout.logosize_slider.get()),y:= 100 * round(self.layout.logosize_slider.get())))
+                img.paste(logo, (1000 - int(x/2),1000 - int(y/2)),logo)
+            except:
+                print("Error: invalid logo file")
+
             return img
 
         elif self.layout.filetype_box.get() == ".svg":
@@ -96,9 +108,9 @@ class MainWindow:
                 )
             )
 
-        make_sure_directory_exists(TEMP_DIR)
+        self.logo_buffer = io.BytesIO()
         logo = Image.open(logo_path)
-        logo.save(f"{TEMP_DIR}/logo.png")
+        logo.save(self.logo_buffer, logo.format)
         self.update_ui()
 
 
@@ -119,13 +131,12 @@ class MainWindow:
             self.layout.logosize_slider_label.place(relx=2, rely=0.92, anchor=tkinter.CENTER)       
 
         if self.layout.preview_checkbox.get() == 1:
-            make_sure_directory_exists(TEMP_DIR)
-            
+
             self.app.root.geometry("1200x600")
             self.layout.settings_frame.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
             self.layout.preview_frame.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
             self.create_qr("temp")
-            self.layout.preview_image.configure(image= tkinter.PhotoImage(file=f"{TEMP_DIR}/temp.png"))
+            self.layout.preview_image.configure(image= tkinter.PhotoImage(data=self.img_buffer.getvalue()))
         elif self.app.root.geometry != "600x600":
             self.app.root.geometry("600x600")
             self.layout.settings_frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
